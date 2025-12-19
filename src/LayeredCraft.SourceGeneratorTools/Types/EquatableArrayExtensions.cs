df5255d1342e6c0335f9c3084f4f69439fb2cdc1
Jonas Ha <jonas-ha@outlook.com>
2025-12-19 09:19:40 -0500

    refactor(source-generator-tools): update EquatableArrayExtensions namespace and method format
    - Changed `namespace` of `EquatableArrayExtensions` to `System.Collections.Generic` for consistent usage.
    - Refactored `ToEquatableArray` method to use extension syntax with better clarity.

--- src/LayeredCraft.SourceGeneratorTools/Types/EquatableArrayExtensions.cs
+++ src/LayeredCraft.SourceGeneratorTools/Types/EquatableArrayExtensions.cs
@@ -1,20 +1,22 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
+using LayeredCraft.SourceGeneratorTools.Types;
 
-namespace LayeredCraft.SourceGeneratorTools.Types;
+namespace System.Collections.Generic;
 
 /// <summary>
 /// Extension methods for creating <see cref="EquatableArray{T}"/> instances.
 /// </summary>
 public static class EquatableArrayExtensions
 {
-    /// <summary>
-    /// Creates an <see cref="EquatableArray{T}"/> from the provided sequence.
-    /// </summary>
-    /// <typeparam name="T">The element type.</typeparam>
-    /// <param name="enumerable">The source sequence.</param>
-    /// <returns>An <see cref="EquatableArray{T}"/> containing the sequence elements.</returns>
-    public static EquatableArray<T> ToEquatableArray<T>(this IEnumerable<T> enumerable)
-        where T : IEquatable<T> => new(enumerable.ToArray());
+    extension<T>(IEnumerable<T> enumerable)
+        where T : IEquatable<T>
+    {
+        /// <summary>
+        /// Creates an <see cref="EquatableArray{T}"/> from the provided sequence.
+        /// </summary>
+        /// <typeparam name="T">The element type.</typeparam>
+        /// <param name="enumerable">The source sequence.</param>
+        /// <returns>An <see cref="EquatableArray{T}"/> containing the sequence elements.</returns>
+        public EquatableArray<T> ToEquatableArray() => new(enumerable.ToArray());
+    }
 }
